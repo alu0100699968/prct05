86e3c4f00930c7b3547fe61f756d80dae6d9583a
Sergio Rodríguez <alu0100699968@ull.edu.es>
2015-10-29 14:23:08 +0000

    Añadido método para simplificar y su test correspondiente

--- lib/racional.rb
+++ lib/racional.rb
@@ -24,4 +24,22 @@ class Racional
   def /(otro)
     Racional.new(@num * otro.dem, @dem * otro.num)
   end
+
+  def gcd(u, v)
+    u, v = u.abs, v.abs
+    while v > 0
+      u, v = v, u % v
+    end
+    u
+  end
+
+  def simplificar
+    gcdd = gcd(@num, @dem)
+    if gcdd != 0
+      otro_num = @num / gcdd
+      otro_dem = @dem / gcdd
+    end
+    Racional.new(otro_num, otro_dem)
+  end
+
 end
--- test/tc_racional.rb
+++ test/tc_racional.rb
@@ -7,10 +7,18 @@ class TestRacional < Test::Unit::TestCase
   def setup
     @test1 = Racional.new(3,2)
     @test2 = Racional.new(2,5)
+    @test3 = Racional.new(6,4)
   end
 
   def test_simple
     assert_equal("(3/2)", @test1.to_s)
+  end
+
+  def test_simplificar
+    assert_equal("(3/2)", @test3.simplificar.to_s)
+  end
+
+  def test_operaciones
     assert_equal("(19/10)", (@test1 + @test2).to_s)
     assert_equal("(11/10)", (@test1 - @test2).to_s)
     assert_equal("(6/10)", (@test1 * @test2).to_s)
